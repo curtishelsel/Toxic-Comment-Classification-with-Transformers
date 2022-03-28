04e736e80e59d0601ec92b163410502eace2d8fa
Curtis Helsel <curtisdhelsel@gmail.com>
2022-03-28 15:36:52 -0400

    Add subtype chart

---
+++ src/data/subtype_charts.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../../data/external/train.csv')
+
+subtypes = ['severe_toxicity', 'obscene', 'identity_attack', 'insult', 'threat']
+
+# Removes all columns except those listed in subtypes
+subtype = df[subtypes]
+
+# Creates a new dataframe from the counts of each column
+subtype = subtype[subtype > 0].count()
+    
+subtype.plot(kind='bar', title='Subtype Attribute Count', 
+                xlabel='Subtype', ylabel='Count')
+
+plt.savefig('../../reports/figures/subtype.png', bbox_inches = "tight")
