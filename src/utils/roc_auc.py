8ef5e29502b342f2e58a7af9d93dbcb6d0ce0ec1
Curtis Helsel <curtisdhelsel@gmail.com>
2022-04-16 09:37:36 -0400

    Add header file and remove ROC plot function

--- src/utils/roc_auc.py
+++ src/utils/roc_auc.py
@@ -1,35 +1,38 @@
+# Utility functions for evaluating area under the curve and the reciever
+# operating characteristics curve for the models
+# CAP6640 - Spring 2022  
+#   
+# Portions of this code are modified from this tutorial:
+# https://skimai.com/fine-tuning-bert-for-sentiment-analysis/
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.metrics import accuracy_score, roc_curve, auc
 
-def get_auc_CV(model, X_train_tfidf, y_train):
-    """
-    Return the average AUC score from cross-validation.
-    """
+# Return the average AUC score from cross-validation
+def get_auc_CV(model, X_train, y_train):
+
     # Set KFold to shuffle data before the split
     kf = StratifiedKFold(5, shuffle=True, random_state=1)
 
     # Get AUC scores
-    auc = cross_val_score(model, X_train_tfidf, y_train, scoring="roc_auc", cv=kf)
+    auc = cross_val_score(model, X_train, y_train, scoring="roc_auc", cv=kf)
 
     return auc.mean()
 
-def evaluate_roc(probs, y_true, model_name):
-    """
-    - Print AUC and accuracy on the test set
-    - Plot ROC
-    @params    probs (np.array): an array of predicted probabilities with shape (len(y_true), 2)
-    @params    y_true (np.array): an array of the true values with shape (len(y_true),)
-    """
-    preds = probs[:, 1]
-    fpr, tpr, threshold = roc_curve(y_true, preds)
+# Evaluates the accuracy and plots the ROC curve of the model 
+def evaluate_roc(probabilities, y_true, model_name):
+
+    # Gets values from the second class and set that as the positive class
+    predictions = probabilities[:, 1]
+    fpr, tpr, threshold = roc_curve(y_true, predictions)
     roc_auc = auc(fpr, tpr)
     print(f'AUC: {roc_auc:.4f}')
        
     # Get accuracy over the test set
-    y_pred = np.where(preds >= 0.5, 1, 0)
-    accuracy = accuracy_score(y_true, y_pred)
+    predictions = np.where(predictions >= 0.5, 1, 0)
+    accuracy = accuracy_score(y_true, predictions)
     print(f'Accuracy: {accuracy*100:.2f}%')
     
     # Plot ROC AUC
@@ -42,14 +45,3 @@ def evaluate_roc(probs, y_true, model_name):
     plt.ylabel('True Positive Rate')
     plt.xlabel('False Positive Rate')
     plt.savefig('../reports/figures/' + model_name + '_roc_auc.png')
-
-
-def plot_auc_alpha(res, model_name):
-
-    plt.plot(res)
-    plt.title('AUC vs. Alpha')
-    plt.xlabel('Alpha')
-    plt.ylabel('AUC')
-    plt.savefig('../reports/figures/' + model_name + '_auc_alpha.png')
-
-
