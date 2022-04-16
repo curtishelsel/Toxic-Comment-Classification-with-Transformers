65e0cbc996e74f1b381a193c5b319b29c73620a3
Curtis Helsel <curtisdhelsel@gmail.com>
2022-04-16 13:13:58 -0400

    Update naming convention for forward and class init

--- src/models/bertclassifier.py
+++ src/models/bertclassifier.py
@@ -3,33 +3,18 @@ import torch.nn as nn
 from transformers import BertModel
 
 class BertClassifier(nn.Module):
-    def __init__(self, freeze_bert=False):
+    def __init__(self):
         super(BertClassifier, self).__init__()
 
-        bert_output = 768
-        hidden_nodes = 256
-        output = 2
-
         self.bert = BertModel.from_pretrained('bert-base-uncased')
-
-        # Fully connected layer
-        self.fully_connected = nn.Sequential(
-            nn.Linear(768, 256),
-            nn.ReLU(),
-            nn.Linear(256, output)
-        )
-
-        if freeze_bert:
-            for param in self.bert.parameters():
-                param.requires_grad = False
-        
+        self.dropout = nn.Dropout(p = 0.3)
+        self.linear = nn.Linear(self.bert.config.hidden_size, 2)
+    
     def forward(self, input_ids, attention_mask):
 
-        outputs = self.bert(input_ids=input_ids,
-                            attention_mask=attention_mask)
-        
-        last_state = outputs[0][:, 0, :]
-
-        out = self.fully_connected(last_state)
+        out = self.bert(input_ids, attention_mask)
+        out = out[1]
+        out = self.dropout(out)
+        out = self.linear(out)
 
         return out
